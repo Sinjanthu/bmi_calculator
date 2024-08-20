43341743d59d69732a51a676b6752cc504c21a8e
sinjanthu <102690976+Sinjanthu@users.noreply.github.com>
2024-08-20 16:12:15 +0200

    v0,2

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _InputPageState extends State<InputPage> {
       ),
       body: const Center(
         child: Text(
-          'Body Text Calculator v11',
+          'Body Text Calculator vk11',
           // style: TextStyle(color: Colors.white),
         ),
       ),
